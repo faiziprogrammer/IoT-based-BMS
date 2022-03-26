10ffd99bf06a5c64b3af5f729d2191fcd3e057fc
Zameera Saleem <93503745+zameera-saleem@users.noreply.github.com>
2022-03-26 05:00:17 -0700

    Add files via upload

---
+++ edit-ultrasonic.ino
@@ -0,0 +1,34 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN 2
+#define ECHO_PIN 0
+#define MAX_DISTANCE 300
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+
+delay(150);
+int uS = sonar.ping();
+if (uS==0)
+{
+Serial.println("MAX: resetting sensor");
+pinMode(ECHO_PIN, OUTPUT);
+delay(150);
+digitalWrite(ECHO_PIN, LOW);
+delay(150);
+pinMode(ECHO_PIN, INPUT);
+delay(150);
+}
+else
+{
+Serial.print(" ");
+Serial.print("Ping: ");
+Serial.print(uS / US_ROUNDTRIP_CM);
+Serial.println("cm");
+}
+}
---
+++ mqtt3.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#define LED D0
+const char* ssid = "ABDULLAH";
+const char* password =  "03165888116";
+const char* mqtt_server = "192.168.100.48";
+const int mqtt_port = 1883;
+WiFiClient espClient;
+PubSubClient client(espClient);
+void setup() 
+{
+  pinMode(LED, OUTPUT);
+  pinMode(D1,INPUT_PULLUP);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.print("Connected to WiFi :");
+  Serial.println(WiFi.SSID());
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(MQTTcallback);
+  while (!client.connected()) 
+  {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266"))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+  client.subscribe("esp/test");
+}
+void MQTTcallback(char* topic, byte* payload, unsigned int length) 
+{
+  Serial.print("Message received in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  String message;
+  for (int i = 0; i < length; i++) 
+  {
+    message = message + (char)payload[i];
+  }
+  Serial.print(message);
+  if (message == "on") 
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else if (message == "off") 
+  {
+    digitalWrite(LED, LOW);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+}
+void loop() 
+{
+  if(digitalRead(D1)==0)
+  {
+  client.publish("esp/test1", "Hello from ESP8266");
+  delay(1000);
+  }
+  else;
+  client.loop();
+}
